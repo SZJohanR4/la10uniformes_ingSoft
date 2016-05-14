3b175b91c2814ac0eb5bb65ccb102530988d0d87
SZJohanR4 <johan_szr@hotmail.com>
2016-05-13 22:04:28 -0500

    codigo base

---
+++ app/src/main/java/ingsoftware/la10uniformes_ingsoft/Cliente.java
@@ -0,0 +1,12 @@
+package ingsoftware.la10uniformes_ingsoft;
+
+/**
+ * Created by Desktop on 13/05/2016.
+ */
+public class Cliente extends Usuario {
+
+
+    public Cliente(String idUsuario, String nombre, String documento, String email, String contraseña, String rol) {
+        super(idUsuario, nombre, documento, email, contraseña, rol);
+    }
+}
---
+++ app/src/main/java/ingsoftware/la10uniformes_ingsoft/Main.java
@@ -0,0 +1,7 @@
+package ingsoftware.la10uniformes_ingsoft;
+
+/**
+ * Created by Desktop on 13/05/2016.
+ */
+public class Main {
+}
