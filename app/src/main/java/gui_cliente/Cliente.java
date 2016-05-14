0eb06ff498915913a3a98346fd6eb7f0f8a2f681
SZJohanR4 <johan_szr@hotmail.com>
2016-05-14 15:51:19 -0500

    Creacion del paquete gui_cliente interfaz de logeo

--- app/src/main/java/gui_cliente/Cliente.java
+++ app/src/main/java/gui_cliente/Cliente.java
@@ -1,16 +1,17 @@
-package ingsoftware.la10uniformes_ingsoft;
+package gui_cliente;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+
+import ingsoftware.la10uniformes_ingsoft.R;
 
 /**
  * Created by Desktop on 13/05/2016.
  */
 public class Cliente extends AppCompatActivity {
 
-    public Cliente() {
-    }
+
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
