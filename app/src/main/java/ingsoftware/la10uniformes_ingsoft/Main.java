bfff4398927caaff090641b9061488815ace5009
SZJohanR4 <johan_szr@hotmail.com>
2016-05-13 22:45:37 -0500

    codigo definitivo

--- app/src/main/java/ingsoftware/la10uniformes_ingsoft/Main.java
+++ app/src/main/java/ingsoftware/la10uniformes_ingsoft/Main.java
@@ -1,7 +1,9 @@
 package ingsoftware.la10uniformes_ingsoft;
 
 /**
- * Created by Desktop on 13/05/2016.
+ * Created by JohanSZ on 13/05/2016.
  */
 public class Main {
+
+    //TODO CODIGO LOGICO AQUI :V
 }
